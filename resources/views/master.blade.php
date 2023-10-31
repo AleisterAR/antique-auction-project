f589f2716f30a3a1ae49bb472655b5b34de18b57
Thu Ta <ladiessman23@gmail.com>
2023-10-31 22:11:45 +0630

    new design

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home</title>
+    <title>Timeless Treasuria</title>
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
     @vite('resources/js/app.js')
