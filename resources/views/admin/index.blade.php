89329d47ef044c0087f44d959e02aeb04fba597d
Thu Ta <ladiessman23@gmail.com>
2023-11-19 20:15:02 +0630

    adding new files and a few changes

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -38,122 +38,27 @@
                      id="sidenavAccordion">
                     <div class="sb-sidenav-menu">
                         <div class="nav">
-                            <div class="sb-sidenav-menu-heading">Entries</div>
-                            <a class="nav-link collapsed"
-                               data-bs-toggle="collapse"
-                               data-bs-target="#collapseLayouts"
-                               href="#"
-                               aria-expanded="false"
-                               aria-controls="collapseLayouts">
+                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
                                 <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-                                Administrators
+                                Entries
                                 <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                             </a>
-                            <div class="collapse"
-                                 id="collapseLayouts"
-                                 data-bs-parent="#sidenavAccordion"
-                                 aria-labelledby="headingOne">
+                            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                                 <nav class="sb-sidenav-menu-nested nav">
-                                    <a class="nav-link"
-                                       href="#">Registration</a>
-                                    <a class="nav-link"
-                                       href="layout-sidenav-light.html">List</a>
+                                    <a class="nav-link" href="layout-static.html">Registration</a>
                                 </nav>
                             </div>
-                            <a class="nav-link collapsed"
-                               data-bs-toggle="collapse"
-                               data-bs-target="#collapseLayouts1"
-                               href="#"
-                               aria-expanded="false"
-                               aria-controls="collapseLayouts">
+                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts1" aria-expanded="false" aria-controls="collapseLayouts">
                                 <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
                                 Experts
                                 <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                             </a>
-                            <div class="collapse"
-                                 id="collapseLayouts1"
-                                 data-bs-parent="#sidenavAccordion"
-                                 aria-labelledby="headingOne">
+                            <div class="collapse" id="collapseLayouts1" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                                 <nav class="sb-sidenav-menu-nested nav">
-                                    <a class="nav-link"
-                                       href="{{ route('entry-register') }}">Registration</a>
-                                    <a class="nav-link"
-                                       href="layout-sidenav-light.html">List</a>
+                                    <a class="nav-link" href="entry-register.blade.php">Registration</a>
+                                    <a class="nav-link" href="layout-sidenav-light.html">List</a>
                                 </nav>
                             </div>
-                            <a class="nav-link collapsed"
-                               data-bs-toggle="collapse"
-                               data-bs-target="#collapsePages"
-                               href="#"
-                               aria-expanded="false"
-                               aria-controls="collapsePages">
-                                <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
-                                Random Bullshit
-                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                            </a>
-                            <div class="collapse"
-                                 id="collapsePages"
-                                 data-bs-parent="#sidenavAccordion"
-                                 aria-labelledby="headingTwo">
-                                <nav class="sb-sidenav-menu-nested nav accordion"
-                                     id="sidenavAccordionPages">
-                                    <a class="nav-link collapsed"
-                                       data-bs-toggle="collapse"
-                                       data-bs-target="#pagesCollapseAuth"
-                                       href="#"
-                                       aria-expanded="false"
-                                       aria-controls="pagesCollapseAuth">
-                                        Authentication
-                                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                                    </a>
-                                    <div class="collapse"
-                                         id="pagesCollapseAuth"
-                                         data-bs-parent="#sidenavAccordionPages"
-                                         aria-labelledby="headingOne">
-                                        <nav class="sb-sidenav-menu-nested nav">
-                                            <a class="nav-link"
-                                               href="login.html">Login</a>
-                                            <a class="nav-link"
-                                               href="register.html">Register</a>
-                                            <a class="nav-link"
-                                               href="password.html">Forgot Password</a>
-                                        </nav>
-                                    </div>
-                                    <a class="nav-link collapsed"
-                                       data-bs-toggle="collapse"
-                                       data-bs-target="#pagesCollapseError"
-                                       href="#"
-                                       aria-expanded="false"
-                                       aria-controls="pagesCollapseError">
-                                        Error
-                                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                                    </a>
-                                    <div class="collapse"
-                                         id="pagesCollapseError"
-                                         data-bs-parent="#sidenavAccordionPages"
-                                         aria-labelledby="headingOne">
-                                        <nav class="sb-sidenav-menu-nested nav">
-                                            <a class="nav-link"
-                                               href="401.html">401 Page</a>
-                                            <a class="nav-link"
-                                               href="404.html">404 Page</a>
-                                            <a class="nav-link"
-                                               href="500.html">500 Page</a>
-                                        </nav>
-                                    </div>
-                                </nav>
-                            </div>
-                            <div class="sb-sidenav-menu-heading">Addons</div>
-                            <a class="nav-link"
-                               href="charts.html">
-                                <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
-                                Charts
-                            </a>
-                            <a class="nav-link"
-                               href="tables.html">
-                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-                                Tables
-                            </a>
                         </div>
                     </div>
                     <div class="sb-sidenav-footer">
