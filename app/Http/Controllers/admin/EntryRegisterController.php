6446c909110fe48dab1f872dcc24221d8d775826
WaiYanLinn <waiyanlinn.lion@outlook.com>
2023-11-20 20:36:29 +0630

    add entry-register

--- app/Http/Controllers/admin/EntryRegisterController.php
+++ app/Http/Controllers/admin/EntryRegisterController.php
@@ -3,11 +3,35 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Spatie\Permission\Models\Role;
+use Throwable;
 
 class EntryRegisterController extends Controller
 {
     public function __invoke()
     {
-        return view('admin.entry-register');
+        $roles = Role::get();
+        return view('admin.entry-register', compact('roles'));
+    }
+
+    public function store(RegisterRequest $request)
+    {
+        DB::beginTransaction();
+        try {
+            $data = array_merge($request->only(['user_name', 'full_name', 'email', 'phone_number', 'address']), ['password' => bcrypt($request->password)]);
+            $role = Role::findOrFail($request->role);
+            $user = User::create($data);
+            $user->assignRole($role);
+            DB::commit();
+        } catch (Throwable $e) {
+            DB::rollBack();
+            Log::error($e->getMessage());
+        }
+        return to_route('entry-register');
     }
 }
--- app/Http/Controllers/admin/HomeController.php
+++ app/Http/Controllers/admin/HomeController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Item;
+use Spatie\Permission\Models\Role;
+
 class HomeController extends Controller
 {
     public function __invoke()
-    {
+    {   
         $items = Item::with(['images', 'provenance.images'])->get();
         return view('admin.index', compact('items'));
     }
