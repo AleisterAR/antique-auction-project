82fc2e971af15c20cec9aa7f428b7fc9f7d11516
Thu Ta <ladiessman23@gmail.com>
2023-12-06 23:11:29 +0630

    Merge remote-tracking branch 'origin/develop' into develop
    # Conflicts:
    #	resources/js/front/item-detail.js

--- resources/views/user/item/info.blade.php
+++ resources/views/user/item/info.blade.php
@@ -122,41 +122,90 @@
             </div>
         </div>
     </nav>
-    <div class="container mt-5 item-detail-container-box">
+    <div class="container item-detail-container-box">
+        <div class="d-flex justify-content-end sort-by-font">
+            Sort by <div class="dropdown">
+                <button class="dropdown-toggle dropdown-toggle-cus" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Dropdown button
+                </button>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="container mt-4 item-detail-container-box">
         <div class="row">
-            <div class="col-md-3"></div>
-            <div class="col-md-9">
-                <div class="row">
-                    <div class="col-lg-4 col-md-4 mb-4">
-                        <div class="card cus-card">
-                            <img src="https://placekitten.com/300/200" class="card-img-top cus-card" alt="Item 1">
-                            <div class="card-body">
-                                <h5 class="card-title">Item 1</h5>
-                                <p class="card-text">Description of item 1.</p>
-                            </div>
-                        </div>
+            <div class="col-lg-3 col-md-6 mb-4 col-12">
+                <div class="card cus-card">
+                    <img src="https://placekitten.com/300/200" class="card-img-top cus-card" alt="Item 1">
+                    <div class="card-body cus-card-body">
+                        <h5 class="card-title cus-card-title">Francesco Zuccarelli (1702 -1788) - River landscape with shepherds </h5>
+                        <p class="card-text cus-card-current-bid">Current Bid <br>
+                            <span class="card-text cus-card-bid-amount">€ 1,500</span>
+                        </p>
+                        <p class="card-text cus-card-timer">
+                            1 day left
+                        </p>
                     </div>
-                    <div class="col-lg-4 col-md-4 mb-4">
-                        <div class="card cus-card">
-                            <img src="https://placekitten.com/300/200" class="card-img-top cus-card" alt="Item 1">
-                            <div class="card-body">
-                                <h5 class="card-title">Item 1</h5>
-                                <p class="card-text">Description of item 1.</p>
-                            </div>
-                        </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-4 col-12">
+                <div class="card cus-card">
+                    <img src="https://placekitten.com/300/200" class="card-img-top cus-card" alt="Item 1">
+                    <div class="card-body cus-card-body">
+                        <h5 class="card-title cus-card-title">Francesco Zuccarelli (1702 -1788) - River landscape with shepherds </h5>
+                        <p class="card-text cus-card-current-bid">Current Bid <br>
+                            <span class="card-text cus-card-bid-amount">€ 1,500</span>
+                        </p>
+                        <p class="card-text cus-card-timer">
+                            1 day left
+                        </p>
                     </div>
-                    <div class="col-lg-4 col-md-4 mb-4">
-                        <div class="card cus-card">
-                            <img src="https://placekitten.com/300/200" class="card-img-top cus-card" alt="Item 1">
-                            <div class="card-body">
-                                <h5 class="card-title">Item 1</h5>
-                                <p class="card-text">Description of item 1.</p>
-                            </div>
-                        </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-4 col-12">
+                <div class="card cus-card">
+                    <img src="https://placekitten.com/300/200" class="card-img-top cus-card" alt="Item 1">
+                    <div class="card-body cus-card-body">
+                        <h5 class="card-title cus-card-title">Francesco Zuccarelli (1702 -1788) - River landscape with shepherds </h5>
+                        <p class="card-text cus-card-current-bid">Current Bid <br>
+                            <span class="card-text cus-card-bid-amount">€ 1,500</span>
+                        </p>
+                        <p class="card-text cus-card-timer">
+                            1 day left
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-4 col-12">
+                <div class="card cus-card">
+                    <img src="https://placekitten.com/300/200" class="card-img-top cus-card" alt="Item 1">
+                    <div class="card-body cus-card-body">
+                        <h5 class="card-title cus-card-title">Francesco Zuccarelli (1702 -1788) - River landscape with shepherds </h5>
+                        <p class="card-text cus-card-current-bid">Current Bid <br>
+                            <span class="card-text cus-card-bid-amount">€ 1,500</span>
+                        </p>
+                        <p class="card-text cus-card-timer">
+                            1 day left
+                        </p>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
+    <div class="container mt-4">
+        <nav aria-label="Page navigation example">
+            <ul class="pagination d-flex justify-content-center">
+                <li class="page-item"><a class="page-link cus-page-link" href="#"><i class="bi bi-caret-left-fill"></i> Previous</a></li>
+                <li class="page-item"><a class="page-link cus-page-link" href="#">1</a></li>
+                <li class="page-item"><a class="page-link cus-page-link" href="#">2</a></li>
+                <li class="page-item"><a class="page-link cus-page-link" href="#">3</a></li>
+                <li class="page-item"><a class="page-link cus-page-link" href="#">Next <i class="bi bi-caret-right-fill"></i></a></li>
+            </ul>
+        </nav>
     </div>
     <footer class="footer">
         <div class="container first-footer">
