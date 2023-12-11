f5579899d8722fba948d265c8ff6f1762473bf7c
Thu Ta <ladiessman23@gmail.com>
2023-12-11 22:36:19 +0630

    Merge remote-tracking branch 'origin/develop' into develop
    # Conflicts:
    #	resources/js/front/item-detail.js

---
+++ resources/views/components/user/footer.blade.php
@@ -0,0 +1,66 @@
+<footer class="footer">
+    <div class="container first-footer">
+        <div class="row">
+            <div class="footer-col">
+                <ul>
+                    <h4>about</h4>
+                    <li>Welcome to our antique and vintage auction house! We have been working for years to provide you
+                        with unique furniture, jewelry, fine arts and other antique items. We value these old and
+                        fragile pieces of history and do our best to longer their life. We regularly provide our clients
+                        with seamless experience, so you could become a happy owner of beautiful objects.
+                    </li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <ul>
+                    <h4>get support</h4>
+                    <li><a href="#">FAQ</a></li>
+                    <li><a href="#">account</a></li>
+                    <li><a href="#">payment methods</a></li>
+                    <li><a href="#">refunds</a></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <ul>
+                    <h4>contact</h4>
+                    <li><a href="#">Contact us</a></li>
+                    <li><a href="#">live chat</a></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <ul>
+                    <h4>follow us</h4>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="second-footer">
+        <div class="container">
+            <div class="p-4 pb-0">
+                <section class="p-3 pt-0 container">
+                    <div class="row d-flex align-items-center">
+                        <div class="col-md-7 col-lg-8 text-start">
+                            <span class="footer-brand">Timeless Treasuria.</span>
+                            <div class="footer-pp-col">
+                                    <span class="footer-pp">All Right Reserved.
+                                        <a class="footer-pp-a"
+                                           href="#">Privacy Policy</a></span>
+                            </div>
+                        </div>
+                        <div class="col-md-5 col-lg-4 ml-lg-0 text-end s-media-btn">
+                            <a class="btn btn-outline-light m-1"
+                               class="text-white"
+                               role="button"><i class="bi bi-facebook"></i></a>
+                            <a class="btn btn-outline-light m-1"
+                               class="text-white"
+                               role="button"><i class="bi bi-twitter-x"></i></a>
+                            <a class="btn btn-outline-light m-1"
+                               class="text-white"
+                               role="button"><i class="bi bi-instagram"></i></a>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
+</footer>
