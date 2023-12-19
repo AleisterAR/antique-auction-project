a7884ace74d1d3c1c5ceb103f936800b368c9e6e
WaiYanLinn <waiyanlinn.lion@outlook.com>
2023-12-19 22:39:24 +0630

    fixed bug

--- app/Http/Controllers/User/ItemInfoController.php
+++ app/Http/Controllers/User/ItemInfoController.php
@@ -10,12 +10,12 @@ class ItemInfoController extends Controller
     public function __invoke()
     {
         $search = request()->search;
-        $items = Item::when($search, function ($query, $search) {
-            $query->where('name', 'like', '%' . $search . '%')
-                ->orWhereHas('category', function ($query) use ($search) {
-                    $query->where('name', 'like', '%' . $search . '%');
-                });
-        })->with(['category', 'auction.currentBid'])->paginate(5);
+        $items = Item::with(['category', 'auction.currentBid'])->when($search, function ($query, $search) {
+                $query->where('name', 'like', '%' . $search . '%')
+                    ->orWhereHas('category', function ($query) use ($search) {
+                        $query->where('name', 'like', '%' . $search . '%');
+                    });
+            })->paginate(5);
         $items->appends(request()->query());
         return view('user.item.info', compact('items'));
     }
