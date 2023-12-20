75fd018524e9252c886122b2497009eccc0b48d7
Thu Ta <ladiessman23@gmail.com>
2023-12-20 20:06:57 +0630

    Merge branch 'develop' of https://github.com/AleisterAR/antique-auction-project into develop

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -1,6 +1,90 @@
 @extends('master')
 
 @section('content')
+    <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="/storage/img.png" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="/storage/wallpaperflare.com_wallpaper.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="/storage/wallpaperflare.com_wallpaper.jpg" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+    <div class="container mt-4 item-detail-container-box">
+        <div class="text-center">
+            <p class="featured-font">Featured Auctions</p>
+        </div>
+        <div class="row">
+            <div class="col-lg-3 col-md-6 mb-4 col-12">
+                <div class="card cus-card">
+                    <img src="https://placekitten.com/300/200" class="card-img-top cus-card" alt="Item 1">
+                    <div class="card-body cus-card-body">
+                        <h5 class="card-title cus-card-title">Francesco Zuccarelli (1702 -1788) - River landscape with shepherds </h5>
+                        <p class="card-text cus-card-current-bid">Current Bid <br>
+                            <span class="card-text cus-card-bid-amount">€ 1,500</span>
+                        </p>
+                        <p class="card-text cus-card-timer">
+                            1 day left
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-4 col-12">
+                <div class="card cus-card">
+                    <img src="https://placekitten.com/300/200" class="card-img-top cus-card" alt="Item 1">
+                    <div class="card-body cus-card-body">
+                        <h5 class="card-title cus-card-title">Francesco Zuccarelli (1702 -1788) - River landscape with shepherds </h5>
+                        <p class="card-text cus-card-current-bid">Current Bid <br>
+                            <span class="card-text cus-card-bid-amount">€ 1,500</span>
+                        </p>
+                        <p class="card-text cus-card-timer">
+                            1 day left
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-4 col-12">
+                <div class="card cus-card">
+                    <img src="https://placekitten.com/300/200" class="card-img-top cus-card" alt="Item 1">
+                    <div class="card-body cus-card-body">
+                        <h5 class="card-title cus-card-title">Francesco Zuccarelli (1702 -1788) - River landscape with shepherds </h5>
+                        <p class="card-text cus-card-current-bid">Current Bid <br>
+                            <span class="card-text cus-card-bid-amount">€ 1,500</span>
+                        </p>
+                        <p class="card-text cus-card-timer">
+                            1 day left
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-4 col-12">
+                <div class="card cus-card">
+                    <img src="https://placekitten.com/300/200" class="card-img-top cus-card" alt="Item 1">
+                    <div class="card-body cus-card-body">
+                        <h5 class="card-title cus-card-title">Francesco Zuccarelli (1702 -1788) - River landscape with shepherds </h5>
+                        <p class="card-text cus-card-current-bid">Current Bid <br>
+                            <span class="card-text cus-card-bid-amount">€ 1,500</span>
+                        </p>
+                        <p class="card-text cus-card-timer">
+                            1 day left
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="masthead">
         <div class="container">
             <div class="masthead-style">
