97711934e687cc85b945c7010283a2487945f3d1
Thu Ta <ladiessman23@gmail.com>
2023-12-23 00:31:32 +0630

    Merge branch 'develop' of https://github.com/AleisterAR/antique-auction-project into develop

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -1,49 +1,16 @@
 @extends('master')
 
 @section('content')
-    <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-            <div class="carousel-item active" data-bs-interval="5000">
-                <img src="{{ asset('static-images/static_image1.jpg')}}" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Globally Available</h5>
-                    <h5>Antiques</h5>
-                    <br>
-                    <p>A place to appreciate culture of other countries in one go!</p>
-                    <p>Antiques from all over the globe gathered in one place!</p>
-                    <br><br>
-                    <a class="btn-cus btn-view-now" href="{{ route('user.item.info') }}">
-                        VIEW NOW!
-                    </a>
-                </div>
-            </div>
-            <div class="carousel-item" data-bs-interval="5000">
-                <img src="{{ asset('static-images/static_image2.jpg')}}" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Globally Available</h5>
-                    <h5>Antiques</h5>
-                    <br>
-                    <p>A place to appreciate culture of other countries in one go!</p>
-                    <p>Antiques from all over the globe gathered in one place!</p>
-                    <br><br>
-                    <a class="btn-cus btn-view-now" href="{{ route('user.item.info') }}">
-                        VIEW NOW!
-                    </a>
-                </div>
-            </div>
-            <div class="carousel-item" data-bs-interval="5000">
-                <img src="{{ asset('static-images/static_image3.jpg')}}" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Globally Available</h5>
-                    <h5>Antiques</h5>
-                    <br>
-                    <p>A place to appreciate culture of other countries in one go!</p>
-                    <p>Antiques from all over the globe gathered in one place!</p>
-                    <br><br>
-                    <a class="btn-cus btn-view-now" href="{{ route('user.item.info') }}">
-                        VIEW NOW!
-                    </a>
-                </div>
+    <div class="masthead2">
+        <div class="container">
+            <div class="masthead-style">
+                <h1 class="boasting-1">
+                    Globally Available Items
+                </h1>
+                <p class="masthead-style2">Antiques from all around the world!</p>
+                <a class="btn-cus btn-view-now" href="{{ route('user.item.info') }}">
+                    VIEW NOW!
+                </a>
             </div>
         </div>
     </div>
