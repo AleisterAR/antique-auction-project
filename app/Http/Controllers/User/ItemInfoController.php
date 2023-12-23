458d3779f1793f3e07fb27aa8d6f0fc267890562
WaiYanLinn <waiyanlinn.lion@outlook.com>
2023-12-23 22:03:22 +0630

    fixed

--- app/Http/Controllers/User/ItemInfoController.php
+++ app/Http/Controllers/User/ItemInfoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\Item;
 
 class ItemInfoController extends Controller
@@ -10,13 +11,22 @@ class ItemInfoController extends Controller
     public function __invoke()
     {
         $search = request()->search;
-        $items = Item::with(['category', 'auction.currentBid'])->when($search, function ($query, $search) {
-                $query->where('name', 'like', '%' . $search . '%')
-                    ->orWhereHas('category', function ($query) use ($search) {
-                        $query->where('name', 'like', '%' . $search . '%');
-                    });
-            })->paginate(5);
+        $sort = (int)request()->sort === 1 ?  'asc' : 'desc';
+        $items = Item::with(['category', 'auction.currentBid', 'image'])
+            ->when($search, function ($query, $search) {
+                $query->where('name', 'like', '%' . $search . '%')->orWhere(function ($query) use ($search) {
+                    $categories = Category::where('name', 'like', '%' . $search . '%')
+                        ->pluck('id')
+                        ->toArray();
+                    $query->whereIn('category_id', $categories);
+                });
+            })->when($sort, function ($query, $sort) {
+                $query->leftJoin('auctions', 'items.id', '=', 'auctions.item_id')
+                    ->orderBy('auctions.initial_price', $sort);
+            })->paginate(10);
+
         $items->appends(request()->query());
+
         return view('user.item.info', compact('items'));
     }
 }
---
+++ resources/views/admin/master.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible"
+          content="ie=edge">
+    <title>Timeless Treasuria</title>
+    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js"
+            crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css"
+          rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"></script>
+    @vite('resources/js/app.js')
+    @vite('resources/js/scripts.js')
+</head>
+
+<body>
+    <div id="modal">
+        <x-user.modal.categories />
+        <x-user.modal.login />
+        <x-user.modal.register />
+        <x-user.modal.auction />
+    </div>
+    @vite('resources/js/main.js')
+    @yield('content')
+    @yield('script')
+</body>
+
+</html>
